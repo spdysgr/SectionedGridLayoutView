48faa496ccea1a9585e255b8c02574180922ce89
spdysgr <spdysgr@gmail.com>
2020-12-12 20:14:18 +0900

    Rebuild home fragment using ViewBinding and put a fragment ui component.

--- app/src/main/java/net/spdysgr/sectionedview/ui/home/HomeFragment.kt
+++ app/src/main/java/net/spdysgr/sectionedview/ui/home/HomeFragment.kt
@@ -8,11 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
 import net.spdysgr.sectionedview.R
+import net.spdysgr.sectionedview.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var viewBinding: ViewBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,11 +24,9 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        viewBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return viewBinding.root
     }
 }
