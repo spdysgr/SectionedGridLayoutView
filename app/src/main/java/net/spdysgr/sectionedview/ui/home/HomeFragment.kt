9394c471e8f04c76c4fe5d23b94fa278f4cd935e
spdysgr <spdysgr@gmail.com>
2020-12-12 22:19:03 +0900

    Delete unuse imports.

--- app/src/main/java/net/spdysgr/sectionedview/ui/home/HomeFragment.kt
+++ app/src/main/java/net/spdysgr/sectionedview/ui/home/HomeFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
-import net.spdysgr.sectionedview.R
 import net.spdysgr.sectionedview.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
